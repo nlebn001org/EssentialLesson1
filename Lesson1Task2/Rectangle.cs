7d336d3c75054537337bd260d1a8279d3fca731d
Nikita ZIBOROV <Nikita.ZIBOROV@livenau.net>
2021-10-31 10:43:14 +0200

    Все сделал круто ) молодец

--- Lesson1Task2/Rectangle.cs
+++ Lesson1Task2/Rectangle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lesson1Task2
+﻿namespace Lesson1Task2
 {
     class Rectangle
     {
